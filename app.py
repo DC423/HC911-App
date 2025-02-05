245808fdfbf290fdf3c7d2f81a2b17ffbcff8b7c
Stephen Hilt <6362482+sjhilt@users.noreply.github.com>
2025-02-05 14:54:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -218,9 +218,6 @@ def register():
 def active_incidents():
     api_url = "https://hc911-proxy.onrender.com/api/proxy" # LOL thanks willsims
         headers = {
-        "Content-Type": "application/json",
-        "X-Frontend-Auth": "my-secure-token",  # Required for security
-        "Origin": "https://www.hamiltontn911.gov",  # Helps with CORS and verification
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.60"
     }
     try:
